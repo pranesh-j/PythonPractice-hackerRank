d9620b1e66c57d4d74b08e2e22ef07db89c60837
Pranesh Jahagirdar <44323581+pranesh-j@users.noreply.github.com>
2022-09-19 16:58:41 +0530

    Update (01) List comprehensions.py

--- Basic Data Types/(01) List comprehensions.py
+++ Basic Data Types/(01) List comprehensions.py
@@ -3,6 +3,6 @@ y = int(input())
 z = int(input())
 n = int(input())
 
-arr = []
-result = [arr.append([i, j, k]) for i in range(x + 1) for j in range(y+1) for k in range(z+1) if i+j+k != n]
-print(arr)
+list = []
+result = [list.append([i, j, k]) for i in range(x + 1) for j in range(y+1) for k in range(z+1) if i+j+k != n]
+print(list)
